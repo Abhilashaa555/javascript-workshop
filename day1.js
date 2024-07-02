410f2e4e02137510793ef4fb59ae6d09a8faa20c
Abhilasha555 <aabhilashaa00@gmail.com>
2024-07-02 15:01:34 +0545

    first commit

---
+++ day1.js
@@ -0,0 +1,17 @@
+console.log("Hello WOrld")
+
+const friend1='Shirsak'
+const friend2='Abhilasha'
+const friend2='Abhilekha'
+
+const friends=['Shirsak' 'Abhilasha' 'Abhilekha']
+
+const name='Shirsak Rai'
+const age 22
+const address= 'Pokhara'
+
+const me{
+    name 'Shirsak',
+    age 22,
+    address 'Pokhara'
+}
